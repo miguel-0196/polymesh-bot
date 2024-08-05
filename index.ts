b7a306a1083f03c6b1973d67a9d6011d0183961d
miguel-0196 <mike.wazowski0196@gmail.com>
2024-08-05 09:02:36 -0500

    Update bot

--- index.ts
+++ index.ts
@@ -1,32 +1,18 @@
+// https://web3auth.io/docs/connect-blockchain/other/polymesh
 import { LocalSigningManager } from "@polymeshassociation/local-signing-manager";
 import { Polymesh, BigNumber } from "@polymeshassociation/polymesh-sdk";
 
 require("dotenv").config();
 
-enum RPC {
-  Testnet = "wss://testnet-rpc.polymesh.live",
-  Mainnet = "wss://mainnet-rpc.polymesh.network",
-}
-
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-
 const main = async () => {
-  const network = process.argv[2] || "test";
-  const node = network == "test" ? RPC.Testnet : RPC.Mainnet;
-  const receiver = process.env[`RECEIVER_${network.toUpperCase()}`];
-
-  console.log(`Starting...`);
+  const receiver = process.env.RECEIVER_KEY!;
   const signingManager = await LocalSigningManager.create({
-    accounts: [
-      {
-        mnemonic: process.env.TARGET_MNEMONIC!, // most mnemonics are 12 words
-      },
-    ],
+    accounts: [{mnemonic: process.env.TARGET_MNEMONIC!},],
   });
 
-  console.log(`Connecting to ${network}net...`);
+  console.log(`Connecting...`);
   const polyClient = await Polymesh.connect({
-    nodeUrl: node,
+    nodeUrl: process.env.RPC_URL!,
     signingManager,
   });
 
@@ -35,31 +21,27 @@ const main = async () => {
       process.stdout.write('.');
       const balance = await polyClient.accountManagement.getAccountBalance();
   
-      if (!balance.free.isLessThan(new BigNumber(0.1))) {
-        console.log(`\nBalance: ${balance.free.toString()}`);
-    
-        const nonce = await polyClient.accountManagement.getSigningAccount()?.getCurrentNonce();
+      if (!balance.free.isLessThan(new BigNumber(2.1))) {
         const polyxTransferTx = await polyClient.network.transferPolyx(
           {
             amount: balance.free.minus(0.1),
-            to: receiver!,
+            to: receiver,
             memo: "",
-          },
-          {
-            nonce,
           }
+          /*,{
+              nonce, = await polyClient.accountManagement.getSigningAccount()?.getCurrentNonce();
+          }*/
         );
       
         try {
-          console.log("Trasfering POLYX...");
+          console.log(`\nTrasfering ${balance.free.toString()} POLYX...`);
           await polyxTransferTx.run();
-          console.log("Completed.", "^_^");
+          console.log("Completed.");
         } catch (e) {
           console.log("Error in transfer transaction!", e);
-        }  
+        }
+        console.log(JSON.stringify(polyxTransferTx.receipt?.events));
       }
-
-      // await sleep(100);
     } catch (err) {
       console.error("Error!", err);
     }
